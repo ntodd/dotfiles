5419d14ea4236b3cb6fb5d07ce76168128f367f3
Nate Todd <ntodd@weigandconstruction.com>
2025-04-15 09:35:03 -0400

    Add git aliases for managing merged branches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,3 +4,7 @@ alias gco='git checkout'
 alias gb='git branch'
 alias g='git status --short'
 alias glog="git log --graph --pretty=format':%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset'"
+# Git branch - show all merged local branches
+alias gbm="git branch --merged | grep -vE '^\*|main|master|develop'"
+# Git branch - delete all local merged branches
+alias gbdm="gbm | xargs -n 1 git branch -d"
